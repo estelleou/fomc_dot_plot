190a6b980174c0d5ff25bcb6895b670c0a967b62
Estelle Ou <39562358+estelleou@users.noreply.github.com>
2022-03-16 15:01:09 -0400

    Update fomc_dot_plot.R
    updated script to not incorporate 2021 data

--- fomc_dot_plot.R
+++ fomc_dot_plot.R
@@ -1,23 +1,24 @@
 library(tidyverse)
 
-
 raw_data <-
-  readxl::read_xlsx("filepath")
+  readxl::read_xlsx("R/grid1_yw2kfuqk.xlsx")
 
 new_data <-
-  readxl::read_xlsx("filepath")
+  readxl::read_xlsx("R/new_data.xlsx")
 
 referenc_data <-
   raw_data %>% 
   pivot_longer(-Target) %>% 
-  filter(!is.na(value))
+  filter(!is.na(value)) %>% 
+  filter(name != "2021")
 
 new_reference_data <-
   new_data %>% 
   pivot_longer(-Target) %>% 
-  filter(!is.na(value))
+  filter(!is.na(value)) %>% 
+  filter(name != "2021")
 
-
+#code for cleaning data into observations
 dup_data <- list()
 for (n in 1:nrow(referenc_data)) {  
   
@@ -29,7 +30,7 @@ for (n in 1:nrow(referenc_data)) {
   
 }
 
-#code for cleaning new data
+#code for cleaning new data into observations
 new_dup_data <- list()
 for (n in 1:nrow(new_reference_data)) {  
   
@@ -47,21 +48,21 @@ ggplot()+
                  select(-value) %>% 
                  mutate(name = ifelse(name == "Longer Term", "2025", name)) %>% 
                  mutate(name = as.Date(name, "%Y")) %>% 
-                 mutate(name = name-450), 
+                 mutate(name = name-530), 
                aes(x= name, y = Target, group = name), 
-               binaxis = "y", stackdir = "center", dotsize = 0.3, fill = "grey", color = "grey") +
+               binaxis = "y", stackdir = "center", dotsize = 0.4, fill = "grey", color = "grey") +
   geom_dotplot(data =  new_dup_data %>% 
                  select(-value) %>% 
                  mutate(name = ifelse(name == "Longer Term", "2025", name)) %>%  
                  mutate(name = as.Date(name, "%Y")) %>% 
-                mutate(name = name-255), 
+                 mutate(name = name-340), 
                aes(x= name, y = Target, group = name), 
-               binaxis = "y", stackdir = "center", dotsize = 0.3,  fill = "red", color = "red") +
+               binaxis = "y", stackdir = "center", dotsize = 0.4,  fill = "red", color = "red") +
   labs(x = "Projection Year End", y = "Implied Fed Funds Target Rate", 
-       title = "September (grey) vs. December (red) FOMC Dot Plot Projections \n(midpoint of target range for fed funds rate)") +
-  scale_x_date( date_break = "1 year", date_label = c("Longer Term", "2021", "2022", "2023", "2024")) +
-  scale_y_continuous(lim = c(0, 3.5), breaks = seq(0,3.5, 0.5))+
+       title = "December 2021 (grey) vs. March 2022 (red) FOMC Dot Plot Projections \n(midpoint of target range for fed funds rate)") +
+  scale_x_date( date_break = "1 year", date_label = c("Longer Term", "2022", "2023", "2024")) +
+  scale_y_continuous(breaks=seq(0,4.0, 0.50))+
   theme_bw()+
-  theme(plot.title = element_text(hjust = 0.5), 
+  theme(plot.title = element_text(hjust = 0.5),
         axis.text = element_text(face = "bold"))
-ggsave("test.png")
+ggsave("dot_plot.png")
